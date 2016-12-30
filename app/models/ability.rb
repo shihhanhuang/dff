368ebfc09b47c54e92678557b0927cfdc0efdc4a
Shih-Han Huang <shihhan.huang@gmail.com>
2016-12-30 10:34:29 +0100

    Admin rights set

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,9 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
-      #if user.admin?
-      #   can :manage, :all
-      #else
+      if user.admin?
+         can :manage, :all
+      else
          can :read, :all
       end
     #
@@ -28,5 +28,5 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-  #end
+  end
 end
