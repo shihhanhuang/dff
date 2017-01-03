80014fcfe013c49b12007bc9a84edc35598b09cf
Shih-Han Huang <shihhan.huang@gmail.com>
2017-01-03 14:31:25 +0100

    Reordered Javascript files

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,13 @@
+
+// Accordian
+<script>
+  var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+  }
+	}
+</script>
