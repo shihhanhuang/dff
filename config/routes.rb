ad4788e4bbbb2ba4282e250b162929563f004008
Shih-Han Huang <shihhan.huang@gmail.com>
2017-01-03 21:24:03 +0100

    Styling changes to nav bar, added new index page

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   resources :blogs
   get 'static_pages/index'
-  root 'static_pages#aboutproject'
+  root 'static_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
