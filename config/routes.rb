035182c304412afbac18897c0fe9c2c09fdcfb00
Shih-Han Huang <shihhan.huang@gmail.com>
2017-01-04 15:54:29 +0100

    Added Contact section and Mail Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/thank_you'
+
   devise_for :users
   resources :users
   get 'static_pages/conversions'
@@ -15,6 +17,8 @@ Rails.application.routes.draw do
   resources :blogs
   get 'static_pages/index'
   root 'static_pages#index'
+
+  post 'static_pages/thank_you'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
